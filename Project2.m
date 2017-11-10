f76cd57b0ffcb8c562ff1b80989aeaf7187e2284
cef39 <cef39@pitt.edu>
2017-11-10 13:09:19 -0500

    created and displayed various lowpass and highpass filters in frequency domain

--- Project2.m
+++ Project2.m
@@ -1,76 +1,118 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Problem 2.1
 
-open gaussKernel.m
-
-w = gaussKernel(60, 25);
-
-testPattern = double(imread('testpattern.tif'));
-imshow(uint8(testPattern))
-pause
-
-blur = filter2(w, testPattern);
-
-
-blurm = blur - min(min(blur));
-% scale image
-blurs = 255*(blurm./max(max(blurm)));
-
-%imshow(uint8(blur))
-%pause
-imshow(uint8(blurs))
-pause
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Problem 2.2
-
-%% Part a
-
-
-blurrymoon = imread('blurrymoon.tif');
-
-%% Gaussian blurring kernel
-kernel = fspecial('Gaussian', 32, 8);
-
-%% apply blurring kernel to image to get blurred image
-blurredImage = imfilter(blurrymoon, kernel, 'replicate');
-
-%% create unsharp mask by subtracting smoothed image from original image
-unsharpMask = blurrymoon - blurredImage;
-
-%% perform unsharp masking by adding unsharp mask to original image
-sharpedImage = blurrymoon + unsharpMask;
-
-imshow(blurrymoon)
-title('image of blurry moon')
-pause
-
-% imshow(unsharpMask)
-% title('unsharp mask of blurrymoon')
+% open gaussKernel.m
+% 
+% w = gaussKernel(60, 25);
+% 
+% testPattern = double(imread('testpattern.tif'));
+% imshow(uint8(testPattern))
+% title('original test pattern image')
+% pause
+% 
+% blur = filter2(w, testPattern);
+% 
+% 
+% blurm = blur - min(min(blur));
+% % scale image
+% blurs = 255*(blurm./max(max(blurm)));
+% 
+% %imshow(uint8(blur))
+% %pause
+% imshow(uint8(blurs))
+% title('test pattern blurred using Gaussian lowpass filter')
+% pause
+% 
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Problem 2.2
+% 
+% %% Part a
+% 
+% 
+% blurrymoon = imread('blurrymoon.tif');
+% 
+% %% Gaussian blurring kernel
+% kernel = fspecial('Gaussian', 32, 8);
+% 
+% %% apply blurring kernel to image to get blurred image
+% blurredImage = imfilter(blurrymoon, kernel, 'replicate');
+% 
+% %% create unsharp mask by subtracting smoothed image from original image
+% unsharpMask = blurrymoon - blurredImage;
+% 
+% %% perform unsharp masking by adding unsharp mask to original image
+% sharpedImage = blurrymoon + unsharpMask;
+% 
+% imshow(blurrymoon)
+% title('image of blurry moon')
+% pause
+% 
+% % imshow(unsharpMask)
+% % title('unsharp mask of blurrymoon')
+% % pause
+% 
+% imshow(sharpedImage)
+% title('sharpened image of blurry moon')
+% pause
+% 
+% 
+% %% Part b
+% 
+% highboost = blurrymoon + 2*unsharpMask;
+% 
+% imshow(highboost)
+% title('highboost filtering of blurry moon')
+% pause
+% 
+% %% Part c
+% 
+% laplaceKernel = [0 -1 0; -1 4 -1; 0 -1 0];
+% 
+% laplaceFilteredImage = filter2(laplaceKernel, blurrymoon);
+% 
+% laplaceSharpened = blurrymoon + uint8(laplaceFilteredImage);
+% 
+% imshow(laplaceSharpened)
+% title('laplace sharpeneing of blurry moon')
 % pause
 
-imshow(sharpedImage)
-title('sharpened image of blurry moon')
-pause
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Problem 2.3
 
+open lpfilterTF.m
 
-%% Part b
+H = lpfilterTF('ideal', 512, 512, 96);
+imshow(H);
+title('ideal lowpass filter')
+pause;
 
-highboost = blurrymoon + 2*unsharpMask;
+H = lpfilterTF('gaussian', 512, 512, 96);
+imshow(H);
+title('gaussian lowpass filter')
+pause;
 
-imshow(highboost)
-title('highboost filtering of blurry moon')
-pause
+H = lpfilterTF('butterworth', 512, 512, [96, 2]);
+imshow(H);
+title('butterworth lowpass filter')
+pause;
 
-%% Part c
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Problem 2.4
 
-laplaceKernel = [0 -1 0; -1 4 -1; 0 -1 0];
+open hpfilterTF.m
 
-laplaceFilteredImage = filter2(laplaceKernel, blurrymoon);
+H = hpfilterTF('ideal', 512, 512, 96);
+imshow(H);
+title('ideal highpass filter')
+pause;
 
-laplaceSharpened = blurrymoon + uint8(laplaceFilteredImage);
+H = hpfilterTF('gaussian', 512, 512, 96);
+imshow(H);
+title('gaussian highpass filter')
+pause;
 
-imshow(laplaceSharpened)
-title('laplace sharpeneing of blurry moon')
-pause
+H = hpfilterTF('butterworth', 512, 512, [96, 2]);
+imshow(H);
+title('butterworth highpass filter')
+pause;
---
+++ hpfilterTF.m
@@ -0,0 +1,40 @@
+function H = hpfilterTF( type, P, Q, param )
+
+switch type
+    case 'ideal'
+        D0 = param;
+        H = ones(P, Q);
+        for u = 1:P
+            for v = 1:Q
+                D = sqrt((u - P/2).^2 + (v - Q/2).^2);
+                if (D <= D0)
+                    H(u, v) = 0;
+                else
+                    H(u,v) = 1;
+                end
+            end
+        end
+    case 'gaussian'
+        D0 = param;
+        H = ones(P, Q);
+        for u = 1:P
+            for v = 1:Q
+                D = sqrt((u - P/2).^2 + (v - Q/2).^2);
+                H(u,v) = 1 - exp(-(D.^2)/(2.*(D0.^2)));
+            end
+        end
+    case 'butterworth'
+        D0 = param(1, 1);
+        n = param(1, 2);
+        H = ones(P, Q);
+        for u = 1:P
+            for v = 1:Q
+                D = sqrt((u - P/2).^2 + (v - Q/2).^2);   
+                H(u,v) = 1/(1 + (D0/D).^(2*n));  
+            end
+        end
+    otherwise
+        disp('Not a supported function. Please use "ideal", "gaussian", or "butterworth"')
+        H = zeros(P, Q);
+        return;
+end
